740462fbb272a61a31091f7a327f3bf9774582a4
NouranFayez <nouran_ahmed269@hotmail.com>
2024-11-28 20:57:36 +0200

    session 1 js

---
+++ Javascript/session1/js/script.js
@@ -0,0 +1,91 @@
+console.log("javascript")
+
+// var variableName = variableValue 
+
+var firstName = "Nouran"
+
+var num = 10
+
+var isGraduated = true
+var isGraduated = false
+
+// console.log(isGraduated)
+
+var h 
+
+// console.log(h)
+
+h = "text"
+
+// console.log(h)
+
+var s = null
+
+// console.log(s)
+
+// concatnate (+)
+
+var middleName = "Ahmed"
+
+var lastName = "fayez"
+
+var fullName = firstName + " " + middleName + " " + lastName
+
+var fullName = `${firstName} ${middleName} ${lastName}`
+
+
+
+// console.log(fullName)
+
+
+//  Mathematical Operators (Assignment) ==> + - / * ** % ++ --
+
+var num1 = 10
+var num2 = 20
+
+var sum = num1 + num2
+var sub = num2 - num1
+
+// console.log(sum)
+// console.log(sub)
+
+// ++ (increment) 
+
+var r = 20
+// console.log(r++)
+// console.log(r)
+
+// console.log(++r)
+// console.log(++r)
+// console.log(++r)
+// console.log(++r)
+
+// console.log(r)
+
+
+// -- decrement
+
+// console.log(address)
+
+var t = 30
+
+console.log(t--)
+console.log(t) // 29
+
+console.log(--t) // 28
+
+var g = 20 / firstName
+var g = 20 + firstName
+
+console.log(g)
+
+
+
+
+
+
+
+
+
+
+
